4b6a47f82145a9c76852625a91f1293f963a523e
Daniel Rodriguez <dannyjose1112@hotmail.com>
2019-07-10 19:04:16 -0600

    [UPDATED] DAM

--- src/app/config.module.ts
+++ src/app/config.module.ts
@@ -1,4 +1,4 @@
-export const path:any = { path:"http://localhost/CODE/backend/public", token:'TOKEN' }
+export const path:any = { path:"http://localhost/CODE/p2p_encuestas/backend/public", token:'TOKEN' }
 // export const path:any = { path:"http://backend.itsme.gt/ticketera/public", token:'TOKEN' }
 // export const path:any = { path:"https://www.comwme.com/backend/public/index.php", token:'TOKEN' }
 // export const path:any = { path:"http://backend.foxylabs.xyz/presentacion/public/api", token:'TOKEN' }
